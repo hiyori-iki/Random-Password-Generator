7379e08a4dcfd7ad47a6e1babe3dc2494d824736
hiyori-iki <dhannugg@gmail.com>
2022-10-17 00:32:29 +0530

    Updates passswords without refreshing page

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let passwordEl1=document.getElementById("Password1")
 let passwordEl2=document.getElementById("Password2")
 let length=characters.length
-let isGenerated=false
-console.log(length)
 function generatePassword()
 {
-    if(isGenerated===false)
-    {
-        for(let i=0;i<14;i++)
-      {
-        let index=Math.floor(Math.random()*length)
-        passwordEl1.innerText+=characters[index]
-        index=Math.floor(Math.random()*length)
-        passwordEl2.innerText+=characters[index]
-      }
-    }
-   isGenerated=true
-
+  let generatedPassword1=""
+  let generatedPassword2=""
+    for(let i=0;i<14;i++)
+  {
+      let index=Math.floor(Math.random()*length)
+      generatedPassword1+=characters[index]
+      index=Math.floor(Math.random()*length)
+      generatedPassword2+=characters[index]
+  }
+  passwordEl1.innerText=generatedPassword1
+  passwordEl2.innerText=generatedPassword2
 }
+
