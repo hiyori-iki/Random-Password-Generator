a2322d52e72f05bf52ede49786d4b3f7e933cad8
hiyori-iki <dhannugg@gmail.com>
2022-10-16 11:12:29 +0530

    Updated password generator button

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let passwordEl1=document.getElementById("Password1")
 let passwordEl2=document.getElementById("Password2")
 let length=characters.length
+let isGenerated=false
 console.log(length)
 function generatePassword()
 {
-    for(let i=0;i<14;i++)
-   {
-    let index=Math.floor(Math.random()*length)
-    passwordEl1.innerText+=characters[index]
-    index=Math.floor(Math.random()*length)
-    passwordEl2.innerText+=characters[index]
-   }
+    if(isGenerated===false)
+    {
+        for(let i=0;i<14;i++)
+      {
+        let index=Math.floor(Math.random()*length)
+        passwordEl1.innerText+=characters[index]
+        index=Math.floor(Math.random()*length)
+        passwordEl2.innerText+=characters[index]
+      }
+    }
+   isGenerated=true
 
 }
